af4eefb1dc033a141c75898385e5177481cc72eb
huxiaoou <516984451@qq.com>
2025-03-04 16:51:40 +0800

    Update to 20250301

--- run_all.ps1
+++ run_all.ps1
@@ -1,5 +1,5 @@
-$bgn_date = "20250103"
-$stp_date = "20250201"
+$bgn_date = "20250205"
+$stp_date = "20250301"
 
 python main.py --bgn $bgn_date --stp $stp_date --switch macro
 python main.py --bgn $bgn_date --stp $stp_date --switch forex
