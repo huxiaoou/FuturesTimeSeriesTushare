a60eff916b0442fe1e57c687fa6f1113832b1a54
huxiaoou <516984451@qq.com>
2025-07-09 17:29:22 +0800

    Update dates in run_all.ps1 for Jun, 2025.

--- run_all.ps1
+++ run_all.ps1
@@ -1,5 +1,5 @@
-$bgn_date = "20250506"
-$stp_date = "20250601"
+$bgn_date = "20250603"
+$stp_date = "20250701"
 
 python main.py --bgn $bgn_date --stp $stp_date --switch macro
 python main.py --bgn $bgn_date --stp $stp_date --switch forex
